67cebdf6053416a345beac392113315795b2182b
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2024-10-10 18:16:01 +0200

    saved random nums e input nums in arrays

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 //const mapRandomNums = new Map(); 
 const arrRandomNums = [];
 boxesRnums = document.querySelectorAll(".container span strong");
+btn_submit = document.querySelector(".container button");
+var guessNums = [];
+
 
 function generateRandomNums(){
     for(let i=0; i<5; i++){
@@ -12,20 +15,25 @@ function generateRandomNums(){
 function generateInputBox(){
     const inputBox = document.createElement("input");
     inputBox.type = "text";
-    inputBox.value = "testtest";
+    inputBox.placeholder = "guess the number";
     return inputBox;
 }
 function boxesClear(){
     boxesRnums.forEach( (item,index) => {
         //item.innerHTML = null;
-        item.replaceWith(generateInputBox());
+        item = item.replaceWith(generateInputBox());
+        //guessNums.push(item);
     }); 
 }
 
-
 function guessNum(){
-
+    const guessnums = document.querySelectorAll(".container span input");
+    guessnums.forEach( (item,index) => {
+        guessNums.push(item.value);
+    });
+    console.log(guessNums);
 }
+btn_submit.addEventListener("click",guessNum);
 
 function thePlay() {
     generateRandomNums();
@@ -33,7 +41,8 @@ function thePlay() {
         item.innerHTML = arrRandomNums[index];
     }); 
     const theTimeout = setTimeout(boxesClear, 3000);
-
 }
 
 thePlay();
+
+
